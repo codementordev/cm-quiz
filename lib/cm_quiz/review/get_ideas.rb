698febc76a2a5960a89ae84ea3cb51133feed703
Olaide Ojewale <ojewaleolaide@gmail.com>
2018-02-24 21:48:48 -0500

    Fix bug in CmQuiz::Review::GetIdeas
    The current implementation of this class will only make the test pass a few times. Say one or two.
    Once the number of ideas created is more than 10, it begins to fail.
    This behaviour is also caused because we do not delete previously created records from this test api.
    
    The fixes the issue and regardless of the number of times the test is run, this spec should not fail.

--- lib/cm_quiz/review/get_ideas.rb
+++ lib/cm_quiz/review/get_ideas.rb
@@ -3,45 +3,37 @@ require 'date'
 module CmQuiz
   module Review
     class GetIdeas < BaseReview
-      VALID_TIME_DIFF = 60 * 5
 
       def initialize(project_api:)
         @project_api = project_api
         @verb = :get
         @path = '/ideas'
-        @now = Time.now
       end
 
       def run
         jwt, _ = Factory::User.new({
           project_api: @project_api
         }).create
-        idea_payloads = 3.times.map do |i|
-          Factory::Idea.new({
-            project_api: @project_api,
-            jwt: jwt,
-            idea_params: {
-              confidence: (3 + i) % 10 + 1
-            }
-          }).create
-        end
+        idea_payload = Factory::Idea.new({
+          project_api: @project_api,
+          jwt: jwt,
+          idea_params: {
+            confidence: 10
+          }
+        }).create
 
         res = send_get_ideas_request(jwt: jwt)
         res_hash = JSON.parse(res.body)
+        item = res_hash.first unless res_hash.empty?
 
-        idea_payloads.each do |idea_payload|
-          item = res_hash.find { |item| item['id'] == idea_payload['id'] }
-          raise StandardError, "idea not found" unless item
+        raise StandardError, "idea not found" unless item
 
-          expect(item['content']).to eq(idea_payload['content'])
-          expect(item['impact']).to eq(idea_payload['impact'])
-          expect(item['ease']).to eq(idea_payload['ease'])
-          expect(item['confidence']).to eq(idea_payload['confidence'])
-          average_score = (idea_payload['impact'] + idea_payload['ease'] + idea_payload['confidence'])/ 3.0
-          expect(item['average_score']).to be_within(0.1).of(average_score)
-          diff = Time.now - Time.new(item['created_at'])
-          expect(diff).to be <= VALID_TIME_DIFF
-        end
+        expect(item['content']).to eq(idea_payload['content'])
+        expect(item['impact']).to eq(idea_payload['impact'])
+        expect(item['ease']).to eq(idea_payload['ease'])
+        expect(item['confidence']).to eq(idea_payload['confidence'])
+        average_score = (idea_payload['impact'] + idea_payload['ease'] + idea_payload['confidence'])/ 3.0
+        expect(item['average_score'].round(1)).to eq(average_score.round(1))
       end
 
       private
